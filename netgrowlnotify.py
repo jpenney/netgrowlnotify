f3aa03a5739dc5a9e90be30983e379b326d643ac
Jason Penney <jpenney@jczorkmid.net>
2009-08-29 10:08:47 -0400

    added GNTP support

--- netgrowlnotify.py
+++ netgrowlnotify.py
@@ -6,7 +6,6 @@ Copyright (c) Jason Penney, 2009
 """
 
 import socket
-from socket import AF_INET, SOCK_DGRAM, socket
 import os
 import os.path
 import sys
@@ -15,17 +14,79 @@ from optparse import OptionParser
 
 VERSION=0.1
 
-def sendMessage(message, addr):
-    s = socket(AF_INET,SOCK_DGRAM)
-    try:
-        s.sendto(message.payload(), addr)
-    finally:
-        s.close()
+    
+    
+def sendMessage(message, addr, socket_type=socket.SOCK_DGRAM, response_size=None):
+    response = None
+    s = socket.socket(socket.AF_INET,socket_type)
+    if socket_type == socket.SOCK_STREAM:
+        s.connect(addr)
+    s.sendto(message, addr)
+    if response_size != None:
+        response = s.recv(response_size)
+    s.close()
+    return response
 
+def sendGNTPMessage(message,addr):
+    import gntp
+    response = gntp.parse_gntp(sendMessage(message.encode(), addr, socket.SOCK_STREAM,1024))
+
+    if isinstance(response, gntp.GNTPOK):
+        return response
+
+    raise Exception('invalid response\n', response)
+
+def gntpnotify(options):
+    try:
+        import gntp
+    except Exception as e:
+        sys.stderr.write("Unable to load 'gntp' module\n")
+        sys.stderr.write(e)
+        sys.exit(1)
+
+        
+    if options.port == None:
+        options.port = 23053
+
+    addr = (options.host, options.port)
+    
+    #register
+    r = gntp.GNTPRegister()
+    r.add_header('Application-Name',options.name)
+    r.add_notification(options.identifier,True)
+    if options.password:
+        r.set_password(options.password)
+        
+
+    response = sendGNTPMessage(r,addr)
+
+    p =  gntp.GNTPNotice()
+
+    p.add_header('Application-Name',options.name)
+    p.add_header('Notification-Name',options.identifier)
+    p.add_header('Notification-Title',options.title)
+
+    p.add_header('Origin-Software-Name',os.path.basename(sys.argv[0]))
+    p.add_header('Origin-Software-Version',VERSION)
+    
+    if options.password:
+        p.set_password(options.password)
+
+    if options.sticky:
+        p.add_header('Notification-Sticky',options.sticky)
+    if options.priority:
+        p.add_header('Notification-Priority',options.priority)
+    if options.message:
+        options.message = options.message.replace("\n","\\n")
+        p.add_header('Notification-Text',options.message)
+	
+    sendGNTPMessage(p,addr)
+
+        
 def prowlnotify(options):
     try:
         import prowlpy
-    except Error as e:
+    except Exception as e:
         sys.stderr.write("Unable to load 'prowlpy' module\n")
         sys.stderr.write(e)
         sys.exit(1)
@@ -74,7 +135,7 @@ def prowlnotify(options):
 def netgrowlnotify(options):
     try:
         import netgrowl
-    except Error as e:
+    except Exception as e:
         sys.stderr.write("Unable to load 'netgrowl' module\n")
         sys.stderr.write(e)
         sys.exit(1)
@@ -88,7 +149,7 @@ def netgrowlnotify(options):
                                          password=options.password)
     r.addNotification(options.identifier)
 
-    sendMessage(r,addr)
+    sendMessage(r.payload(),addr)
     
     p = netgrowl.GrowlNotificationPacket(application=options.name,
                                          notification=options.identifier,
@@ -98,7 +159,7 @@ def netgrowlnotify(options):
                                          sticky=options.sticky,
                                          password=options.password)
 
-    sendMessage(p,addr)
+    sendMessage(p.payload(),addr)
 
 def parse_time(stime):
     ftime = time.strftime('%Y %j ',time.localtime()) + stime.replace(' ','')
@@ -164,6 +225,10 @@ if __name__ == "__main__":
                       help="Path to file containing Prowl API key",
                       default="")
 
+    # gntp support
+    parser.add_option("-g","--gntp",action="store_true",default=False,
+                      help="Send notification via GNTP (default is to use UDP)")
+
     # time window support
     parser.add_option("","--time-start", default=None, type="string",
                       dest="time_start")
@@ -172,7 +237,11 @@ if __name__ == "__main__":
 
     (options,args) = parser.parse_args()
     options.title = options.title + " " + " ".join(args)
-    options.message = options.message.replace("\\n","\n")
+
+
+    if options.message:
+        options.message = options.message.replace("\\n","\n")
+
 
     if options.time_start != None or options.time_end != None:
         now = time.localtime()
@@ -190,6 +259,8 @@ if __name__ == "__main__":
 
     if options.prowl:
         prowlnotify(options)
+    elif options.gntp:
+        gntpnotify(options)
     else:
         netgrowlnotify(options)
 
